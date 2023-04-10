b948893b789b5024a3d19fcd076854ae3150cc21
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-10 09:40:15 -0500

    following the react docs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+import MyButton from './MyButton';
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
 }
