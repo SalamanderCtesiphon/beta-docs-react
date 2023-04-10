c465e03464f1f8b06d9098f9e23dbcd8279acb84
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-10 10:00:26 -0500

    following tutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
-import './App.css';
-import MyButton from './MyButton';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
+export default function Square() {
+  return <button className="square">X</button>;
 }
 
-export default App;
